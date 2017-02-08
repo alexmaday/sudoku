9d29218f4b472cae1ddd46f6f0b86a0d81c2ae66
Alex Maday <alex.maday@gmail.com>
2017-02-07 20:45:12 -0600

    from codepen

---
+++ js/script.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+  
+  function buildBoard() {
+    var board = document.getElementById('board');
+    
+    for (var i = 0; i < 9; i++) {
+      // build a row
+      var row = document.createElement('div')
+      row.style.classList
+    }
+  }
+};
